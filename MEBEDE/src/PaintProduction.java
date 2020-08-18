4454914a19bcb74603b8e409233ef8f1fcbbe1b8
AdamPanag <adampanag06@gmail.com>
2020-08-18 15:04:30 +0300

    Add a method for each function

--- MEBEDE/src/PaintProduction.java
+++ MEBEDE/src/PaintProduction.java
@@ -6,6 +6,8 @@ import java.util.Random;
 public class PaintProduction {
 	
 	public static void main(String args[]) {
+		
+		PaintProduction pp = new PaintProduction();
 			
 		ArrayList<Order> orders = new ArrayList<>();
 	
@@ -26,11 +28,9 @@ public class PaintProduction {
 		
 		 for (int i = 0; i < orders.size();i++) 
 	      { 
-			 System.out.println(orders.get(i).ID);	      
-	          
-			 System.out.println(orders.get(i).quantity);
-	          
-	         System.out.println(orders.get(i).dark);
+			 System.out.print(orders.get(i).ID + " ");
+			 System.out.print(orders.get(i).quantity + " ");  
+	         System.out.print(orders.get(i).dark + "\n");
 	      }   
 	
 			
@@ -48,59 +48,95 @@ public class PaintProduction {
 		
 			}
 			
-			for (int i = 0; i < transitionTime.length; i++) {
-				for (int j = 0; j < transitionTime[i].length; j++) {
-					System.out.print(transitionTime[i][j] + "  ");
-				}
-				System.out.println();
-			}
 			//start. construction (question A)
-			
-			ArrayList<Order>[] machine = new ArrayList[5];
-			
-			for (int i = 0; i < 5; i++){
-				machine[i]= new ArrayList<Order>();
-			
-			}
-			
-			int k = 0;
-			for (int i=0; i < totOrders; i++) {
-				machine[k].add(orders.get(i));
-				k++;
-				if(k==5) {
-					k = 0;
-				}
-			}
-			
-			double[] timeOfMachine = new double[5];
-			
-			for(int i=0; i<5; i++) {
-				timeOfMachine[i] = 0;
-			}
-			
-			//calculates the time that each machine is working (Question B)
-			for (int i=0; i < 5; i++) {
-				for (int j=0; j < machine[i].size() - 1; j++) {
-					timeOfMachine[i] = timeOfMachine[i] + machine[i].get(j).quantity * 6 
-							+ transitionTime[machine[i].get(j).ID - 1][machine[i].get(j + 1).ID - 1];
 
-					//adds extra 15 mins if the next color is different than the current
-					if(machine[i].get(j).dark != machine[i].get(j + 1).dark) {
-						timeOfMachine[i] += 15 * 60;
-					}
-				}
-				timeOfMachine[i] = timeOfMachine[i] + machine[i].get(machine[i].size() - 1).quantity * 6; //adds the time of the last order of curr machine.
-			}
-				
+			pp.simpleAlgorythm(transitionTime, totOrders, orders);
 			
-			System.out.println();
-			System.out.println();
-			System.out.println("time of machine 1: " + timeOfMachine[0]);
-			System.out.println("time of machine 2: " + timeOfMachine[1]);
-			System.out.println("time of machine 3: " + timeOfMachine[2]);
-			System.out.println("time of machine 4: " + timeOfMachine[3]);
-			System.out.println("time of machine 5: " + timeOfMachine[4]);
+
+	}
 	
+	/*
+	 * The simple solution
+	 */
+	public void simpleAlgorythm(double[][] transitionTime, int totOrders, ArrayList<Order> orders) {
+		
+		PaintProduction pp = new PaintProduction();
+		ArrayList<Order>[] machine = new ArrayList[5];
+		
+		for (int i = 0; i < 5; i++){
+			machine[i]= new ArrayList<Order>();
+		
+		}
+		
+		int k = 0;
+		for (int i=0; i < totOrders; i++) {
+			machine[k].add(orders.get(i));
+			k++;
+			if(k==5) {
+				k = 0;
+			}
+		}
+		
+		double[] timeOfMachine = pp.findOperationTimeOfEachMachine(machine, transitionTime);
+		System.out.println(pp.findMaxOperationTime(timeOfMachine));
+				
+	}
+	
+	/*
+	 * Pleonektikos algorithmos
+	 */
+	public void greedyAlgorythm(double[][] transitionTime, int totOrders, ArrayList<Order> orders) {
+		
+		PaintProduction pp = new PaintProduction();
+		ArrayList<Order>[] machine = new ArrayList[5];
+		
+		for (int i = 0; i < 5; i++){
+			machine[i]= new ArrayList<Order>();
+		
+		}
+		
+	}
+	
+	/*
+	 * Calculates the time that each machine is working
+	 */
+	public double[] findOperationTimeOfEachMachine(ArrayList<Order>[] machine, double[][] transitionTime) {
+		double[] timeOfMachine = new double[5];
+		
+		for(int i=0; i<5; i++) {
+			timeOfMachine[i] = 0;
+		}
+		
+		for (int i=0; i < 5; i++) {
+			for (int j=0; j < machine[i].size() - 1; j++) {
+				timeOfMachine[i] = timeOfMachine[i] + machine[i].get(j).quantity * 6 
+						+ transitionTime[machine[i].get(j).ID - 1][machine[i].get(j + 1).ID - 1];
+
+				//adds extra 15 mins if the next color is different than the current
+				if(machine[i].get(j).dark != machine[i].get(j + 1).dark) {
+					timeOfMachine[i] += 15 * 60;
+				}
+			}
+			timeOfMachine[i] = timeOfMachine[i] + machine[i].get(machine[i].size() - 1).quantity * 6; //adds the time of the last order of curr machine.
+		}
+		
+		return timeOfMachine;
+	
+	}
+	
+	/*
+	 * Finds the max operation time of the machines
+	 */
+	public double findMaxOperationTime(double[] timeOfMachine) {
+		double maxOpTime = timeOfMachine[0];
+		
+		for(int i = 1; i < timeOfMachine.length; i++ ) {
+			if(timeOfMachine[i] > maxOpTime) {
+				maxOpTime = timeOfMachine[i];
+			}
+		}
+		
+		return maxOpTime;
 	}
 	
 	
