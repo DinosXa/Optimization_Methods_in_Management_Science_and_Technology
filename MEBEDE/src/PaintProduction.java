93c3d514a8b421064db49043bbf19e70ca3cf219
loukas pap <t8170101@aueb.gr>
2020-08-19 19:59:04 +0300

    Add Greedy Algorithm

--- MEBEDE/src/PaintProduction.java
+++ MEBEDE/src/PaintProduction.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random; 
+import java.util.Collections;
 
 
 public class PaintProduction {
@@ -23,16 +24,6 @@ public class PaintProduction {
 			Order o = new Order(i + 1, qq , drk);
 			orders.add(o);
 		} 
-		
-		 for (int i = 0; i < orders.size();i++) 
-	      { 
-			 System.out.println(orders.get(i).ID);	      
-	          
-			 System.out.println(orders.get(i).quantity);
-	          
-	         System.out.println(orders.get(i).dark);
-	      }   
-	
 			
 		double[][] transitionTime = new double[totOrders][totOrders];
 			
@@ -47,66 +38,150 @@ public class PaintProduction {
 				}
 		
 			}
-			
-			for (int i = 0; i < transitionTime.length; i++) {
-				for (int j = 0; j < transitionTime[i].length; j++) {
-					System.out.print(transitionTime[i][j] + "  ");
-				}
-				System.out.println();
-			}
-			//start. construction (question A)
-			
-			ArrayList<Order>[] machine = new ArrayList[5];
-			
-			for (int i = 0; i < 5; i++){
-				machine[i]= new ArrayList<Order>();
-			
-			}
-			
-			int k = 0;
-			for (int i=0; i < totOrders; i++) {
-				machine[k].add(orders.get(i));
-				k++;
-				if(k==5) {
-					k = 0;
-				}
-			}
-			
-			double[] timeOfMachine = new double[5];
-			
-			for(int i=0; i<5; i++) {
-				timeOfMachine[i] = 0;
-			}
-			
-			//calculates the time that each machine is working (Question B)
-			for (int i=0; i < 5; i++) {
-				for (int j=0; j < machine[i].size() - 1; j++) {
-					timeOfMachine[i] = timeOfMachine[i] + machine[i].get(j).quantity * 6 
-							+ transitionTime[machine[i].get(j).ID - 1][machine[i].get(j + 1).ID - 1];
 
-					//adds extra 15 mins if the next color is different than the current
-					if(machine[i].get(j).dark != machine[i].get(j + 1).dark) {
-						timeOfMachine[i] += 15 * 60;
-					}
-				}
-				timeOfMachine[i] = timeOfMachine[i] + machine[i].get(machine[i].size() - 1).quantity * 6; //adds the time of the last order of curr machine.
-			}
-				
-			
-			System.out.println();
-			System.out.println();
-			System.out.println("time of machine 1: " + timeOfMachine[0]);
-			System.out.println("time of machine 2: " + timeOfMachine[1]);
-			System.out.println("time of machine 3: " + timeOfMachine[2]);
-			System.out.println("time of machine 4: " + timeOfMachine[3]);
-			System.out.println("time of machine 5: " + timeOfMachine[4]);
-	
+			simpleAlgo(transitionTime, totOrders, orders);
+			greedyAlgo(transitionTime, totOrders, orders);
 	}
 	
 	
-	// question C . na katataksoume tis parageleies me vasi to xroma k meta min(tranisistion time).
+	public static void simpleAlgo(double[][] transitionTime, int totOrders, ArrayList<Order> orders) {
+		PaintProduction pp = new PaintProduction();	
+		ArrayList<Order>[] machine = new ArrayList[5];
+			
+		for (int i = 0; i < 5; i++){
+			machine[i]= new ArrayList<Order>();
+		}
 		
+		int k = 0;
+		for (int i=0; i < totOrders; i++) {
+			machine[k].add(orders.get(i));
+			k++;
+			if(k==5) {
+				k = 0;
+			}
+		}
+		
+		double[] timeOfMachine = new double[5];
+		
+		for(int i=0; i<5; i++) {
+			timeOfMachine[i] = 0;
+		}
+		timeOfMachine = pp.findOperationTimeOfEachMachine(machine, transitionTime);
+		
+		System.out.println("\nSimple:");
+		for(int i=0; i < 5; i++) {
+			System.out.printf("Time of machine %s: %s\n", i+1, timeOfMachine[i]);
+		}
+
+	}
+					
+	public static double[] findOperationTimeOfEachMachine(ArrayList<Order>[] machine, double[][] transitionTime) {
+		double[] timeOfMachine = new double[5];
+		
+		for(int i=0; i<5; i++) {
+			timeOfMachine[i] = 0;
+		}
+		
+		for (int i=0; i < 5; i++) {
+			for (int j=0; j < machine[i].size() - 1; j++) {
+				timeOfMachine[i] = timeOfMachine[i] + machine[i].get(j).quantity * 6 
+						+ transitionTime[machine[i].get(j).ID - 1][machine[i].get(j + 1).ID - 1];
+
+				//adds extra 15 minutes if the next color is different than the current
+				if(machine[i].get(j).dark != machine[i].get(j + 1).dark) {
+					timeOfMachine[i] += 15 * 60;
+				}
+			}
+			timeOfMachine[i] = timeOfMachine[i] + machine[i].get(machine[i].size() - 1).quantity * 6; //adds the time of the last order of current machine.
+		}
+		
+		return timeOfMachine;
+	}
+			
+	// Problem C
+	
+	public static void greedyAlgo(double[][] transitionTime, int totOrders, ArrayList<Order> orders) {
+		
+		//Initialize machines' time
+		ArrayList<Order>[] machine = new ArrayList[5];
+		for (int i = 0; i < 5; i++){
+			machine[i]= new ArrayList<Order>();
+		}
+		
+		double[] timeOfMachine = new double[5];
+		for(int i=0; i<5; i++) {timeOfMachine[i] = 0;}
+			
+		//Sorting orders by quantity
+		Collections.sort(orders, (o1, o2) -> Double.compare(o1.quantity, o2.quantity));
+		
+		int iom = 0;
+		double ord1 = 0, ord2 = 0;
+		
+		for (int i=0; i < totOrders-2; i=i+2) {
+			ord1 = orders.get(i).quantity*6 + transitionTime[i][i+1];
+			ord2 = orders.get(i+1).quantity*6 + transitionTime[i+1][i];
+			
+			
+			if(ord1 < ord2) {
+				machine[iom].add( orders.get(i) );
+			}else {
+				machine[iom].add( orders.get(i+1) );
+			}
+			
+			iom = findIdOfMinOperationTime(timeOfMachine);
+			timeOfMachine = findOT(machine, transitionTime);
+			
+			
+		}
+		System.out.println("\nGreedy:");
+		for(int i=0; i < 5; i++) {
+			System.out.printf("Time of machine %s: %s\n", i+1, timeOfMachine[i]);
+		}
+		
+
+	
+	}
+	
+	public static double[] findOT(ArrayList<Order>[] machine, double[][] transitionTime) {
+		double[] timeOfMachine = new double[5];
+		
+		for(int i=0; i<5; i++) {
+			timeOfMachine[i] = 0;
+		}
+		
+		for (int i=0; i < 5; i++) {
+			for (int j=0; j < machine[i].size() - 1; j++) {
+				timeOfMachine[i] = timeOfMachine[i] + machine[i].get(j).quantity * 6 
+						+ transitionTime[machine[i].get(j).ID - 1][machine[i].get(j + 1).ID - 1];
+
+				//adds extra 15 minutes if the next color is different than the current
+				if(machine[i].get(j).dark != machine[i].get(j + 1).dark) {
+					timeOfMachine[i] += 15 * 60;
+				}
+			}
+//			timeOfMachine[i] = timeOfMachine[i] + machine[i].get(machine[i].size() - 1).quantity * 6; //adds the time of the last order of current machine.
+		}
+		
+		return timeOfMachine;
+	}
+	
+	public static int findIdOfMinOperationTime(double[] timeOfMachine) {
+		double minTime = timeOfMachine[0];
+		int mid = 0;
+		for(int i = 1; i < timeOfMachine.length; i++ ) {
+			if(timeOfMachine[i] < minTime) {
+				minTime = timeOfMachine[i];
+				mid = i;
+			}
+		}
+		
+		return mid;
+	}
+	
 }
+	
+	
+	
 
 
 
