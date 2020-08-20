525843bc1af023e869b4d3ad198d5d00dd14433a
DinosXa <Traolele@icloud.com>
2020-08-20 23:30:25 +0300

    Add Greedy Algorithm

--- MEBEDE/src/Machine.java
+++ MEBEDE/src/Machine.java
@@ -1,31 +1,15 @@
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class Machine {
 	
-	public ArrayList<Order> batch = new ArrayList<Order>();
+	//public ArrayList<Order> batch = new ArrayList<Order>();
 	public int machID;
-    public int capacity;
-    public int load;
-    public boolean closed;
-
-    public Machine(int id, int cap)
+    public double SumofTime;
+   
+    public Machine(int id, double sumtime)
     {
         this.machID = id;
-        this.capacity = cap;
-        this.load = 0;
-        this.closed = false;
-        this.batch.clear();
+        this.SumofTime = sumtime;
+        //this.batch.clear();
     }
-
-    public void addOrder(Order order) //Add Order to Machine
-    {
-       batch.add(order);
-       //this.curLocation = Order.ID;
-    }
-
-    public boolean checkIfFits(int quantity) //Check if we have Quantity Violation
-    {
-        return ((load + quantity <= capacity));
-    }
-
 }
--- MEBEDE/src/PaintProduction.java
+++ MEBEDE/src/PaintProduction.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
+import java.util.Collections; 
 import java.util.Arrays;
 import java.util.Random; 
 
 
 public class PaintProduction {
+	
 	
 	public static void main(String args[]) {
 			
@@ -23,16 +25,6 @@ public class PaintProduction {
 			Order o = new Order(i + 1, qq , drk);
 			orders.add(o);
 		} 
-		
-		 for (int i = 0; i < orders.size();i++) 
-	      { 
-			 System.out.println(orders.get(i).ID);	      
-	          
-			 System.out.println(orders.get(i).quantity);
-	          
-	         System.out.println(orders.get(i).dark);
-	      }   
-	
 			
 		double[][] transitionTime = new double[totOrders][totOrders];
 			
@@ -50,10 +42,11 @@ public class PaintProduction {
 			
 			for (int i = 0; i < transitionTime.length; i++) {
 				for (int j = 0; j < transitionTime[i].length; j++) {
-					System.out.print(transitionTime[i][j] + "  ");
+					//System.out.print(transitionTime[i][j] + "  ");
 				}
-				System.out.println();
+				//System.out.println();
 			}
+			
 			//start. construction (question A)
 			
 			ArrayList<Order>[] machine = new ArrayList[5];
@@ -72,6 +65,79 @@ public class PaintProduction {
 				}
 			}
 			
+			
+			ArrayList<Order> darkOrders = createSortedDarkOrders(orders);
+			ArrayList<Order> lightOrders = createSortedLightOrders(orders);
+			
+			FirstFitIncreasingAlgorithm(darkOrders,lightOrders);	
+			
+			calculateOpTime(transitionTime,FirstFitIncreasingAlgorithm(darkOrders,lightOrders));
+			
+		}
+	
+	
+	
+	
+	
+		// dimiourgo 2 taksinomimenous ( kata aujoysa seira) pinakes  me vasi to xroma kai quantity.
+		
+		public static ArrayList <Order> createSortedDarkOrders(ArrayList <Order> orders) {
+			
+			ArrayList<Order> darkOrders = new ArrayList<>();
+			
+			for ( Order element: orders) {
+				if (element.dark == true) {
+					darkOrders.add(element);
+				}				
+			}
+			Collections.sort(darkOrders,(q1, q2) -> Double.compare(q1.quantity,q2.quantity));
+			
+			return darkOrders;
+		}
+		
+		
+		public static ArrayList<Order> createSortedLightOrders(ArrayList <Order> orders) {
+			
+			ArrayList<Order> lightOrders = new ArrayList<>();
+			
+			for ( Order element: orders) {
+				if (element.dark == false) {
+					lightOrders.add(element);
+					
+				}				
+			}
+			Collections.sort(lightOrders,(q1, q2) -> Double.compare(q1.quantity,q2.quantity));	
+			
+			return lightOrders;
+		}
+		
+		
+		// topotheto kathe order sthn amesos epomenh mixani. dld 1 order me 1 mixani, 2 order sth deuterh mixani klp. 		
+		// FisrtFitIncreasingAlgorithm (
+					
+		public static ArrayList<Order>[] FirstFitIncreasingAlgorithm(ArrayList<Order> darkOrders, ArrayList<Order> lightOrders) {
+			
+			ArrayList<Order>[] machine = new ArrayList[5];	
+			for (int i = 0; i < 5; i++){
+				machine[i]= new ArrayList<Order>();		
+			}
+				int k = 0;
+				for ( int i = 0 ; i < darkOrders.size(); i++) {							
+					machine[k].add(darkOrders.get(i));
+					k++;				
+					k = k==5 ? 0 : k;										
+				}
+				for ( int i = 0 ; i < lightOrders.size(); i++) {						
+					machine[k].add(lightOrders.get(i));
+					k++;				
+					k = k==5 ? 0 : k;							
+				}
+				return machine;
+		}
+	
+		
+		public static void calculateOpTime(double[][] transitionTime, ArrayList<Order>[] machine) {
+			
 			double[] timeOfMachine = new double[5];
 			
 			for(int i=0; i<5; i++) {
@@ -82,7 +148,7 @@ public class PaintProduction {
 			for (int i=0; i < 5; i++) {
 				for (int j=0; j < machine[i].size() - 1; j++) {
 					timeOfMachine[i] = timeOfMachine[i] + machine[i].get(j).quantity * 6 
-							+ transitionTime[machine[i].get(j).ID - 1][machine[i].get(j + 1).ID - 1];
+							+ transitionTime[machine[i].get(j).ID-1][machine[i].get(j + 1).ID-1];
 
 					//adds extra 15 mins if the next color is different than the current
 					if(machine[i].get(j).dark != machine[i].get(j + 1).dark) {
@@ -90,21 +156,64 @@ public class PaintProduction {
 					}
 				}
 				timeOfMachine[i] = timeOfMachine[i] + machine[i].get(machine[i].size() - 1).quantity * 6; //adds the time of the last order of curr machine.
-			}
 				
+			}
+			for (int i=0;i<5;i++) {
+				System.out.println(timeOfMachine[i]);
+			}
+		}
+		
+		
+		
+		
+		/*public static void getMinTime(double[][] transitionTime,ArrayList <Order> orders) {
+			double Inverse = 0;
+			int Row = 0;
+			int Column = 0;
+			double Min = transitionTime[1][0];
+			
+			for (int i = 0; i < transitionTime.length; i++) {
+				
+				for (int j = 0; j < transitionTime[i].length; j++) {			
+					
+					if (transitionTime[i][j] < Min && transitionTime[i][j] != 0.0) { //find min time excluding diagonal
+						
+						Min = transitionTime[i][j];
+						Inverse = transitionTime[j][i];
+						Row = i;
+						Column = j;
+						  				
+					}
+				}
+				
+			}*/
+			
+			
+			/*System.out.println();
+			System.out.println("Shortest:"+ Min);
+			System.out.println("Row:"+ Row);
+			System.out.println("Column:"+ Column);
+			
+			System.out.println("id"+orders.get(Row-1).ID);
+			System.out.println("dark"+orders.get(Row-1).dark);	  
+			System.out.println("id"+orders.get(Column-1).ID);
+			System.out.println("dark"+orders.get(Column-1).dark);	
 			
 			System.out.println();
-			System.out.println();
-			System.out.println("time of machine 1: " + timeOfMachine[0]);
-			System.out.println("time of machine 2: " + timeOfMachine[1]);
-			System.out.println("time of machine 3: " + timeOfMachine[2]);
-			System.out.println("time of machine 4: " + timeOfMachine[3]);
-			System.out.println("time of machine 5: " + timeOfMachine[4]);
+			System.out.println("Shortest:"+ Inverse);
+			System.out.println("Row:"+ Column);
+			System.out.println("Column:"+ Row);*/
+				
+			
 	
-	}
+	//}
+		
 	
 	
-	// question C . na katataksoume tis parageleies me vasi to xroma k meta min(tranisistion time).
+	
+	
+	
+	
 		
 }
 
